3ef3bee2e1629d87a563eb0582551291edd0e233
Gustavo Nunes <109112700+guxtaavo@users.noreply.github.com>
2025-03-07 08:53:33 -0300

    Add score and improving the appearance

--- SeriousRush/Assets/Scripts/GameManager.cs
+++ SeriousRush/Assets/Scripts/GameManager.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject gameOverPanel; // Referência ao painel de Game Over
-
+    public GameObject gameOverPanel; // Referï¿½ncia ao painel de Game Over
+    public TMP_Text ScoreText;
+    private int score;
+    private float timer;
+    public static GameManager Instance { get; private set; }
     public void GameOver()
     {
         Debug.Log("GameOver chamado");
@@ -18,5 +22,18 @@ public class GameManager : MonoBehaviour
         gameOverPanel.SetActive(false);
         Time.timeScale = 1f;
         SceneManager.LoadScene("SampleScene");
+    }
+
+    private void UpdateScore()
+    {
+        int scorePerSeconds = 10;
+        timer += Time.deltaTime;
+        score = (int)(timer * scorePerSeconds);
+        ScoreText.text = string.Format("{0:00000}", score);
+    }
+
+    private void Update()
+    {
+        UpdateScore();
     }
 }
